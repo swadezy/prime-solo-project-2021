4ab6ec7d1e706ee2b69e6eab1ceb4b559f8b0878
Sean Wade <smwade1115@gmail.com>
2021-02-08 10:18:00 -0600

    switched user routes to users routes in sql queries to match db structure

--- src/redux/reducers/user.reducer.js
+++ src/redux/reducers/user.reducer.js
@@ -1,4 +1,5 @@
 const userReducer = (state = {}, action) => {
+  console.log('in user reducer, received', action.payload);
   switch (action.type) {
     case 'SET_USER':
       return action.payload;
