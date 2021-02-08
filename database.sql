4e90d66fe9686ac5a0ceaa7c9f0cb7868c2081f4
Sean Wade <smwade1115@gmail.com>
2021-02-08 09:57:33 -0600

    sean initial commit, added env and connected db to pool

--- database.sql
+++ database.sql
@@ -3,8 +3,9 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
+CREATE TABLE "users" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "username" VARCHAR (100) UNIQUE NOT NULL,
+    "password" VARCHAR (100) NOT NULL,
+    "admin" BOOLEAN NOT NULL DEFAULT false
 );
