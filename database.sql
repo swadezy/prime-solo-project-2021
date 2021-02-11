8da049035c4c965b30eae24d6ce8197f18525e20
Sean Wade <smwade1115@gmail.com>
2021-02-11 15:07:19 -0600

    added more robust dummy data to db sql

--- database.sql
+++ database.sql
@@ -22,8 +22,8 @@ CREATE TABLE "types" (
 
 CREATE TABLE "locks" (
 	"id" SERIAL PRIMARY KEY,
-	"nickname" VARCHAR(50) NOT NULL,
 	"user_id" INT REFERENCES "users" NOT NULL,
+	"nickname" VARCHAR(50) NOT NULL,
 	"brand_id" INT REFERENCES "brands",
 	"type_id" INT REFERENCES "types",
 	"num_pins" INT,
@@ -33,7 +33,8 @@ CREATE TABLE "locks" (
 
 CREATE TABLE "pickings" (
 	"id" SERIAL PRIMARY KEY,
-	"lock_id" INT REFERENCES "locks" NOT NULL,
+	"user_id" INT REFERENCES "users" NOT NULL,
+	"lock_id" INT REFERENCES "locks" ON DELETE CASCADE NOT NULL,
 	"success" BOOLEAN NOT NULL,
 	"time_taken" INT,
 	"date" DATE,
@@ -41,24 +42,94 @@ CREATE TABLE "pickings" (
 );
 
 --dummy data
+
+--users to create in client
+--sean / locksport
+--joe / schmo
+--jane / schmane
+
 INSERT INTO "brands" ("brand")
-VALUES ('Master'), ('Schlage'), ('Medeco');
+VALUES ('Abus'), ('American'), ('Brinks'), ('Commando'), ('Kwikset'), ('Master'), ('Sargent'), ('Schlage'), ('Tru-Bolt'), ('Weiser'), ('Yale'), ('Other');
 
 INSERT INTO "types" ("type")
-VALUES ('pin-tumbler'), ('combination'), ('digital');
+VALUES ('Disc Detainer'), ('Lever'), ('Pin Cylinder'), ('Wafer'), ('Warded'), ('Other');
 
-INSERT INTO "locks" ("nickname", "user_id", "brand_id", "type_id", "num_pins", "img_url", "notes")
-VALUES ('lil minkus',1, 2, 1, 3, 'test url', 'testing notes'), ('big chungus',1, 3, 2, 5, 'test url 2', 'testing notes 2');
+--dummy locks for user 1, admin
+INSERT INTO "locks" ("user_id", "nickname", "brand_id", "type_id", "num_pins", "img_url", "notes")
+VALUES
+(1, 'Medium Speckled Padlock', 6, 3, 4, 'http', 'First challenging lock I picked, found at Home Depot'),
+(1, 'Small Blue Padlock', 9, 3, 3, 'http', 'First lock I ever picked!'),
+(1, '(solid) Gold Padlock', 9, 2, 4, 'http', 'My favorite looking lock'),
+(1, 'Shiny Reddit Find', 2, 5, 5, 'http', 'Found on reddit, tricky security pins'),
+(1, 'Practice Lock', 6, 3, 3, 'http', 'Good for showing people the beginnings of picking'),
+(1, 'The Challenger', 8, 1, 6, 'http', 'The toughest lock I own - put this thing in the Pentagon');
 
-INSERT INTO "pickings" ("lock_id", "success", "time_taken", "date", "notes")
-VALUES (1, true, 48, '2021-02-04', 'good picking'), (2, false, 90, '2021-02-08', 'failed');
+--dummy locks for user 2, general user
+INSERT INTO "locks" ("user_id", "nickname", "brand_id", "type_id", "num_pins", "img_url", "notes")
+VALUES
+(2, 'My First Lock', 1, 4, 3, 'http', 'Maybe starting with a wafer lock was a bad idea...'),
+(2, 'Shed Padlock', 10, 2, 4, 'http', 'Maybe moving on to a lever lock was ill advised...'),
+(2, 'Light Breeze', 6, 3, 2, 'http', 'More my speed, a light breeze could open this bad boy');
 
-SELECT "users".username, "locks".nickname FROM "pickings"
-JOIN "locks" ON "pickings".lock_id = "locks".id
-JOIN "brands" ON "locks".brand_id = "brands".id
-JOIN "types" ON "locks".type_id = "types".id
-JOIN "users" ON "locks".user_id = "users".id;
+--dummy locks for user 3, general user
+INSERT INTO "locks" ("user_id", "nickname", "brand_id", "type_id", "num_pins", "img_url", "notes")
+VALUES
+(3, 'LPLs Recommendation', 5, 4, 4, 'http', 'The master himself said this was a good starter lock'),
+(3, 'Junkyard Find', 12, 6, 5, 'http', 'Who even knows what this is?');
+
+--dummy pickings for user 1, admin
+INSERT INTO "pickings" ("user_id", "lock_id", "success", "time_taken", "date", "notes")
+VALUES
+(1, 1, false, 300, '2021-01-13', 'Tried using small hook to single pin pick, got a couple pins I think'),
+(1, 1, false, 300, '2021-01-14', 'Tried using small hook again then got frustrated and raked a bunch'),
+(1, 1, true, 274, '2021-01-19', 'Got it!! Turns out taking a few days off pays off, I guess'),
+(1, 1, true, 185, '2021-01-20', 'Piece of cake now that I got it once'),
+(1, 2, true, 48, '2021-01-11', 'Always nice to start out with the easy one'),
+(1, 2, true, 67, '2021-01-26', 'Literally can do this blind (had my eyes closed the whole time)'),
+(1, 2, true, 17, '2021-02-05', 'World record raking seems like hehe'),
+(1, 3, false, 300, '2021-01-04', 'Looks cooler than it is easy to pick'),
+(1, 3, false, 300, '2021-01-08', 'Maybe this is just for decoration...'),
+(1, 3, false, 300, '2021-01-26', 'Starting to wonder if this thing actually works, or if it works TOO well'),
+(1, 3, false, 300, '2021-01-27', 'Maybe I can sell it.'),
+(1, 4, false, 300, '2021-02-05', 'This security pin is a spooky one. I will get you next time'),
+(1, 4, false, 300, '2021-02-06', 'I did not get it next time.'),
+(1, 4, true, 184, '2021-02-10', 'AYYYY'),
+(1, 5, true, 133, '2021-01-27', 'Showed Jon how lockpicking works - he was into it!'),
+(1, 5, true, 44, '2021-01-28', 'Showed him just how fast you can pick once you get the hang of it'),
+(1, 6, false, 300, '2021-02-10', 'Today is the day. You are going down, Mr. Schlage'),
+(1, 6, false, 300, '2021-02-10', 'I am pretty sure I got 4 pins... It goes 4, 3, 6, 1 - I think...?'),
+(1, 6, false, 300, '2021-02-10', 'Nope - 4, 5, 3, 6... Maybe?'),
+(1, 6, false, 300, '2021-02-10', 'I swear the pins in this thing randomize every time I try it.'),
+(1, 6, false, 300, '2021-02-10', 'I think I am gonna sleep on it at this point'),
+(1, 6, false, 300, '2021-02-10', 'Need a new hobby.');
+
+--dummy pickings for user 2, general user
+INSERT INTO "pickings" ("user_id", "lock_id", "success", "time_taken", "date", "notes")
+VALUES
+(2, 7, false, 600, '2021-02-01', 'Is it supposed to be this hard?'),
+(2, 7, false, 600, '2021-02-01', 'Oh. Apparently wafer locks are not exactly for beginners.'),
+(2, 8, false, 600, '2021-02-04', 'Found a new lock, this should be better...  I hope'),
+(2, 8, false, 600, '2021-02-05', 'Apparently lever locks are not for newbies either. Just my luck'),
+(2, 8, false, 600, '2021-02-11', 'I am gonna crack this someday'),
+(2, 9, false, 600, '2021-02-07', 'Did not quite get it but fairly certain this one is doable'),
+(2, 9, true, 428, '2021-02-07', 'Got it!!! My first successful picking!'),
+(2, 9, true, 298, '2021-02-08', 'Gets so much easier after the first time, wow'),
+(2, 9, true, 185, '2021-02-10', 'I think I can go back to the tougher ones now!');
+
+
+--dummy pickings for user 3, general user
+INSERT INTO "pickings" ("user_id", "lock_id", "success", "time_taken", "date", "notes")
+VALUES
+(3, 10, false, 180, '2021-01-20', 'Followed along with LPLs video, this should be fun!'),
+(3, 10, false, 180, '2021-01-21', 'Had The Good Place on in the background'),
+(3, 10, false, 180, '2021-01-21', 'Watched Parks & Rec on this time, great show'),
+(3, 10, false, 180, '2021-01-22', 'Tried The Sopranos, seems slow but good. Maybe I should focus on the lockpicking?'),
+(3, 10, true, 87, '2021-01-22', 'Turns out I just had to pay attention to the actual picking'),
+(3, 11, false, 180, '2021-02-01', 'Literally found this on the ground. Looks straight out of LotR, pins feel weird'),
+(3, 11, false, 180, '2021-02-03', 'Something fell out of the cylinder? Seems shady, could not even tell if the pins were setting'),
+(3, 11, false, 180, '2021-02-04', 'When I set one of the pins, a strange purple glow briefly enveloped the lock and I could hear a distant unearthly keening. Had The Office on in the background, love this show!');
 
 --drop tables for postico testing
 DROP TABLE "users";
 DROP TABLE "locks", "pickings";
+DROP TABLE "brands", "types";
